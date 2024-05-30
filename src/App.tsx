d2a0037048d1fcb6c04975843b9c8413d412d3b7
Ammar Shah <ammarshah123@gmail.com>
2024-05-30 17:31:33 +0500

    Adds bootstrap and react-bootstrap packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Alert from "react-bootstrap/Alert";
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Alert key="primary" variant="primary">
+      Hello World!
+    </Alert>
+  );
 }
 
 export default App;
